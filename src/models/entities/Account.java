e1a8bfb2b4bd3ab0fc6cf6ede93a92b216674ac2
Antonio Pires <tonan@globo.com>
2020-10-06 11:27:33 -0300

    Projeto iniciado

---
+++ src/models/entities/Account.java
@@ -0,0 +1,44 @@
+package models.entities;
+
+public class Account {
+
+	private Integer number;
+	private String holder;
+	private Double balance;
+	private Double withdrawLimit;
+	
+	public Account(Integer number, String holder, Double balance, Double withdrawLimit) {
+		this.number = number;
+		this.holder = holder;
+		this.balance = balance;
+		this.withdrawLimit = withdrawLimit;
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+
+	public String getHolder() {
+		return holder;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+
+	public Double getWithdrawLimit() {
+		return withdrawLimit;
+	}
+
+	public void setWithdrawLimit(Double withdrawLimit) {
+		this.withdrawLimit = withdrawLimit;
+	}
+	
+	public void deposit(Double amount) {
+		balance+=amount;
+	}
+	public void withdraw(Double amount) {
+		balance-=amount;
+	}
+	
+}
